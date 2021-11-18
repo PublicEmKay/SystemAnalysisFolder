d660e3e0afd5ec5478869720dc0949e5c739eac0
PublicEmKay <94438040+PublicEmKay@users.noreply.github.com>
2021-11-18 19:41:34 +0200

    Дадано коментар

--- Laba01/Laba01.py
+++ Laba01/Laba01.py
@@ -3,15 +3,20 @@
 #Відкриття файлу input.txt для читання
 adress = "/home/emkay/Документи/SystemAnalysisFolder/Laba01/input.txt"
 
-#Створення списку
-list = []
+#Метод повертає вкладений список з файлу
+def listinput(adress):
 
-#Читання інформації з файлу
-with open(adress,'r') as f:
-    for line in f:
-        lines = []
-        for word in line.split():
-            lines.append(word)
-        list.append(lines)
+    #Створення списку
+    list = []
+
+    #Читання інформації з файлу
+    with open(adress,'r') as f:
+        for line in f:
+            lines = []
+            for word in line.split():
+                lines.append(word)
+            list.append(lines)
+    return list
+
 
 
