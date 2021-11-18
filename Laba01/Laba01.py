68f2d24c2b53137141487d36cae2405d657faba3
PublicEmKay <94438040+PublicEmKay@users.noreply.github.com>
2021-11-18 20:13:06 +0200

    Додано кілька методів

--- Laba01/Laba01.py
+++ Laba01/Laba01.py
@@ -18,5 +18,45 @@ def listinput(adress):
             list.append(lines)
     return list
 
+#Метод повертає числове значення, необхіне для вибору методу сортування
+def userinput():
+    n = input('Сортувати за прізвищем(1), сортувати за іменем(2), сортувати по прізвищу(3), сортувати за номером телефону(4):\n')
+    n = int(n)-1
+    return n
+
+#Метод повертає булеве значення
+def userinputsort():
+    n = input('Сортувати за зростанням(1), за спаданням(0):\n')
+    n = int(n)
+    if n == 1:
+        return False
+    else:
+        return True
+
+#Метод повертає адресу файлу
+def userinputfile():
+    n = input('Вкажіть повну адресу файлу для сортування:\n')
+    return n
+
+#Метод виводить текст на консоль
+def outputlist(list):
+    for i in range(len(list)):
+        for j in range(len(list[i])):
+            print(list[i][j], end = ' ')
+        print()
+
+#Цей метод повертає відсортований список
+def sortlist(list, methodsort, sortreverse):
+    listresult = list.sort(key=lambda i: i[methodsort], reverse=sortreverse)
+    return listresult
+adress = userinputfile()
+list = listinput(adress)
+outputlist(list)
+methodsort = userinput()
+sortreverse = userinputsort()
+list = sortlist(list, methodsort, sortreverse)
+print(list)
+
+
 
 
