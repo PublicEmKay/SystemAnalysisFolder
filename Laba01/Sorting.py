51f5348a7fef9325da2ba8322bb9dfd24b6a5c78
PublicEmKay <94438040+PublicEmKay@users.noreply.github.com>
2021-11-24 11:06:45 +0200

    До класу Sorting додано кілька методів

--- Laba01/Sorting.py
+++ Laba01/Sorting.py
@@ -1,5 +1,28 @@
 class Sorting(object):
     def __init__(self):
         """Constructors"""
+        self.methodsort
+        self.sortreverse
 
-        
+    #Метод повертає числове значення, необхіне для вибору методу сортування
+    def userinput(self):
+        n = input('Сортувати за прізвищем(1), сортувати за іменем(2), сортувати по батькові(3), сортувати за номером телефону(4):\n')
+        n = int(n)-1
+        self.methodsort = n
+
+    #Метод повертає булеве значення
+    def userinputsort(self):
+        n = input('Сортувати за зростанням(1), за спаданням(0):\n')
+        n = int(n)
+        if n == 1:
+            self.sortreverse = False
+        else:
+            self.sortreverse = True
+
+    #Повертає атрибут methodsort
+    def get_methodsort(self):
+        return self.methodsort
+
+    #Повертає атрибут sortreverse
+    def get_sortreverse(self):
+        return self.sortreverse
