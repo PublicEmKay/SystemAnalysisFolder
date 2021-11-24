0d8ff103f46e60b7ba11147f7f95c72e0fcce2be
PublicEmKay <94438040+PublicEmKay@users.noreply.github.com>
2021-11-24 11:17:42 +0200

    В класі Output створено методи outputlist та outputlisttofile

--- Laba01/Output.py
+++ Laba01/Output.py
@@ -1,4 +1,19 @@
 class Output(object):
     def __init__(self, list):
         """Constructors"""
-        
+        self.list = list
+    
+    #Метод виводить текст на консоль
+    def outputlist(self):
+        for i in range(len(self.list)):
+            for j in range(len(self.list[i])):
+                print(self.list[i][j], end = ' ')
+            print()
+
+    #Метод виводить вкладений список до файлу
+    def outputlisttofile(self):
+        n = input('Вкажіть повну адресу файлу для запису:\n')
+        with open(n, "w") as file:
+            print(*self.list, file=file, sep="\n")
+            #file.write(",".join(map(str, self.list))+"\n")
+            
